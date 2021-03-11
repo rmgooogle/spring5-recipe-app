7512a373e1f9625eaba2146a02658190415727be
rmgoo <rm.gooogle@gmail.com>
2021-03-11 21:08:36 +0300

    refactoring IndexController

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -1,12 +1,20 @@
 package guru.springframework.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class IndexController {
-    @RequestMapping({"","/","/index"})
-    public String getIndexPage(){
+    @RequestMapping({"","/","/index","xyindex"})
+    public String getIndexPage(HttpServletRequest request, Model model){
+
+        String welcomeMessage = "now in ";
+        model.addAttribute("message",  request.getRequestURL().toString() );
+        model.addAttribute("message2", welcomeMessage + request.getRequestURI());
         return "index";
     }
 }
